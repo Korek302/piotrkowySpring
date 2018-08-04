67af54c0076139b1119f8797e6db9b81d5e88082
Korek302 <jacek_blady@interia.pl>
2018-08-04 18:21:47 +0200

    piotrek

--- src/main/java/com/example/piotrkowySpring/controllers/ChessController.java
+++ src/main/java/com/example/piotrkowySpring/controllers/ChessController.java
@@ -1,15 +1,14 @@
 package com.example.piotrkowySpring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class ChessController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/chess")
     public String index() {
-        return "greeting.html";
+        return "chess.html";
     }
 
 }
